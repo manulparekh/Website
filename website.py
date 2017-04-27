af3346ba1279e2e70e2d1ddffad2cd13b8c93cdd
Manul <manulparekh@gmail.com>
2017-04-27 11:32:56 +0530

    Add port

--- website.py
+++ website.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def webprint():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, port=port, host='0.0.0.0')
